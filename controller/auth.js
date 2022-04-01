680622e00893191f844e2bf601d7bc06f16a89fe
pancho <pancho@pancho.com>
2022-03-31 23:36:01 -0300

    error de datos duplicado

--- controller/auth.js
+++ controller/auth.js
@@ -6,6 +6,19 @@ const crearUsuario = async(req, res = response) => {
 
     const usuario = new Usuario(req.body);
 
+    //const email=usuario.email;
+    const {email} =req.body;
+
+try {
+    //const emailExistente=Usuario.findOne({email:email})
+    const emailExistente = await Usuario.findOne({email});
+    if(emailExistente){
+        return res.status(400).json({
+            ok:false,
+            mge:'correo exixtente '
+        }); 
+    } 
+
     await usuario.save();
 
 
@@ -13,10 +26,19 @@ const crearUsuario = async(req, res = response) => {
 
         ok: true,
         mge: 'Usuario creado',
-    //    dataBody:req.body
+        dataBody:usuario
 
 
     });
+    
+} catch (error) {
+    console.log('conectarse con el administrador' ,error)
+    
+}
+
+
+
+   
 
 
 };
